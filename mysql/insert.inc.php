1abf60244b35af200ebc6d0eb34d8fe8540ec8fb
digistam <digistam@gmail.com>
2015-01-23 10:13:44 +0100

    insert.inc.php aangepast

--- mysql/insert.inc.php
+++ mysql/insert.inc.php
@@ -1,14 +1,14 @@
 <?php
-//gegevens voor toegang
+    //gegevens voor toegang
 
-$host = "127.0.0.1";
-$user = "*****";
-$password = "*****";
-$dbname = "instagram";
-$tablename = "media";
+    $host = "127.0.0.1";
+    $user = "*****";
+    $password = "*****";
+    $dbname = "instagram";
+    $tablename = "media";
 
-//verbinding opbouwen
-$db = mysql_connect($host, $user, $password) or die("verbinding mislukt");
-//database als standaard definiëren
-mysql_select_db($dbname,$db);
+    //verbinding opbouwen
+    $db = mysql_connect($host, $user, $password) or die("verbinding mislukt");
+    //database als standaard definiëren
+    mysql_select_db($dbname,$db);
 ?>
